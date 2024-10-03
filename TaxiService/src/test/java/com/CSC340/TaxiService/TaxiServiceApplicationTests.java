20289283306cd9f8783d96741c9ed7f6c342b4da
cdgibb <cdgibb2015@gmail.com>
2024-10-03 10:29:28 -0400

    Added html prototype

---
+++ TaxiService/src/test/java/com/CSC340/TaxiService/TaxiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.TaxiService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
