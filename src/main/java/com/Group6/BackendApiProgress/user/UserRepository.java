1407a9a8b5e471dc1d224af7f9e9a12350757188
AveryKillian99 <alkillian@uncg.edu>
2024-11-06 22:48:55 -0500

    prototype CRUD api

---
+++ src/main/java/com/Group6/BackendApiProgress/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Group6.BackendApiProgress.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
---
+++ src/test/java/com/Group6/BackendApiProgress/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group6.BackendApiProgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
